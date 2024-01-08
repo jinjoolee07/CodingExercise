92b12b1962bd483cd23498740809f32f147587a1
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-08 18:07:42 +0900

    refactor code

--- examples/example_string_shuffling.rs
+++
@@ -1,8 +0,0 @@
-use coding_exercise::encode::string_shuffling::Solution;
-
-fn main() {
-    let s = String::from("codeleet");
-    let indices = vec![4, 5, 6, 7, 0, 2, 1, 3];
-    let result = Solution::restore_string(&s, &indices);
-    println!("{}", result); // Output: "leetcode"
-}
--- examples/example_subtract_prod_sum.rs
+++ examples/example_subtract_prod_sum.rs
@@ -1,7 +1,7 @@
 use coding_exercise::numbers::subtract_product_sum::Solution;
 
 fn main() {
-    let input = 4421;
+    let input = 234;
     let result = Solution::subtract_product_and_sum(input);
     println!("{:?}", result);
 }
