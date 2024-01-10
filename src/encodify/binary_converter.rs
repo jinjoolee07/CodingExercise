a1dc0271dc48c90daa50b558b9645c5efea20c68
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-10 21:02:10 +0900

    refactor solution for binary_coverter

--- src/encodify/binary_converter.rs
+++ src/encodify/binary_converter.rs
@@ -1,5 +1,5 @@
 // Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>,
@@ -11,30 +11,22 @@ impl ListNode {
     pub fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
-
-    // Function to create a linked list from a vector
-    #[must_use]
-    pub fn from_vec(vec: &[i32]) -> Option<Box<ListNode>> {
-        let mut head = None;
-        let mut current = &mut head;
-
-        for &val in vec {
-            *current = Some(Box::new(ListNode::new(val)));
-            current = &mut current.as_mut()?.next;
-        }
-
-        head
-    }
 }
 
 pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn get_decimal_value(head: &Option<Box<ListNode>>) -> i32 {
+    pub fn get_decimal_value(vec: &[i32]) -> i32 {
         let mut result = 0;
+        let mut current = None;
 
-        let mut current = head;
+        for &val in vec {
+            let _node = ListNode::new(val); // Added underscore to indicate intentional unused variable
+            current = Some(Box::new(ListNode { val, next: current }));
+        }
+
+        let mut current = &current;
         while let Some(node) = current {
             result = (result << 1) + node.val;
             current = &node.next;
