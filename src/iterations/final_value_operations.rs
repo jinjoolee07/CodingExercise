cc0ebdcef5881a34c45da0ef83587acdb0c416ec
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-11 10:20:17 +0900

    add iterations files

---
+++ src/iterations/final_value_operations.rs
@@ -0,0 +1,19 @@
+pub struct Solution;
+
+impl Solution {
+    #[must_use]
+    pub fn final_value_after_operations(operations: Vec<&str>) -> i32 {
+        let mut x = 0;
+
+        for operation in operations {
+            match operation {
+                "++X" | "X++" => x += 1,
+                "--X" | "X--" => x -= 1,
+                _ => {
+                    println!("Invalid operation: {operation}");
+                }
+            }
+        }
+        x
+    }
+}
---
+++ src/iterations/steps_reduce_to_zero.rs
@@ -0,0 +1,18 @@
+pub struct Solution;
+
+impl Solution {
+    #[must_use]
+    pub fn number_of_steps(mut num: i32) -> i32 {
+        let mut steps = 0;
+
+        while num != 0 {
+            if num % 2 == 0 {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps += 1;
+        }
+        steps
+    }
+}
