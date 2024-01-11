5a4dd87002b2ae94746bacb4a4d3c5a0024c854c
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-11 10:20:17 +0900

    add iterations files

---
+++ src/iterations.rs
@@ -0,0 +1,3 @@
+pub mod final_value_operations;
+pub mod steps_reduce_to_zero;
+pub mod target_array_order;
---
+++ src/iterations/target_array_order.rs
@@ -0,0 +1,11 @@
+// pub struct Solution;
+
+// impl Solution {
+//     pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
+//         let mut target = Vec::new();
+
+//         for (&num, &idx) in nums.iter().zip(index.iter()) {
+
+//         }
+//     }
+// }
