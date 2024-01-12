3b3492e10b41f869cb203e24b26fa6a2ad1d1aff
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-12 11:33:56 +0900

    new solutions for iterations

---
+++ examples/example_matches_in_tournament.rs
@@ -0,0 +1,7 @@
+use coding_exercise::iterations::matches_in_tournament::Solution;
+
+fn main() {
+    let input = 7;
+    let result = Solution::number_of_matches(input);
+    println!("{:?}", result);
+}
