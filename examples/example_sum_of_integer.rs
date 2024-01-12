e497036fc949833f5caead2ffb10fd947b90b86b
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-12 15:40:50 +0900

    fixed clippy error for iterations solution

--- examples/example_sum_of_integer.rs
+++ examples/example_sum_of_integer.rs
@@ -1,8 +1,8 @@
 use coding_exercise::sum::of_integer::Solution;
 
 fn main() {
-    let num1 = 12;
-    let num2 = 5;
+    let num1 = -10;
+    let num2 = 4;
     let result = Solution::sum(num1, num2);
     println!("{:?}", result);
 }
