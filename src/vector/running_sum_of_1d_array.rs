17b3cc26f8b91d048a5bc48d620ccc938a5571ce
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-15 17:28:36 +0900

    refactor using standard library

--- src/vector/running_sum_of_1d_array.rs
+++ src/vector/running_sum_of_1d_array.rs
@@ -3,15 +3,11 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn running_sum(nums: &[i32]) -> Vec<i32> {
-        let mut running_sum = vec![0; nums.len()];
-
-        for i in 0..nums.len() {
-            running_sum[i] = if i == 0 {
-                nums[i]
-            } else {
-                running_sum[i - 1] + nums[i]
-            };
-        }
-        running_sum
+        nums.iter()
+            .scan(0, |acc, &x| {
+                *acc += x;
+                Some(*acc)
+            })
+            .collect()
     }
 }
