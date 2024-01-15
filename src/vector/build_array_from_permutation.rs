b236ee219fcfb6ca3284616a2c5fa7ad25b3c6f3
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-15 15:43:24 +0900

    refactor using standard library

--- src/vector/build_array_from_permutation.rs
+++ src/vector/build_array_from_permutation.rs
@@ -3,13 +3,6 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn build_array(nums: &[usize]) -> Vec<usize> {
-        let mut result = Vec::new();
-        let n = nums.len();
-
-        for i in 0..n {
-            result.push(nums[nums[i]]);
-        }
-
-        result
+        nums.iter().map(|&x| nums[x]).collect()
     }
 }
