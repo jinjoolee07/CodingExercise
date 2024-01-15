3be07eeec7f0d793d6d86888c85e7151cc5e0d40
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-15 13:26:16 +0900

    refactor concat using standard library

--- src/vector/concatenation_of_array.rs
+++ src/vector/concatenation_of_array.rs
@@ -3,16 +3,8 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn get_concatenation(nums: &[i32]) -> Vec<i32> {
-        let mut result = Vec::new();
-
-        for &num in nums {
-            result.push(num);
-        }
-
-        for &num in nums {
-            result.push(num);
-        }
-
-        result
+        let mut ans = nums.to_vec();
+        ans.extend_from_slice(nums);
+        ans
     }
 }
