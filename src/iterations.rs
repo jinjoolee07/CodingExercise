f6f38a6d59753b79ecef7047287dc6e5fd2fb31f
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-15 12:22:49 +0900

    nothing to fix for sum solution but fixed errors from most recent main commit

--- src/iterations.rs
+++ src/iterations.rs
@@ -1,3 +1,5 @@
 pub mod final_value_operations;
+pub mod matches_in_tournament;
+pub mod min_moves_seat_all;
 pub mod steps_reduce_to_zero;
 pub mod target_array_order;
--- src/iterations/target_array_order.rs
+++ src/iterations/target_array_order.rs
@@ -1,11 +1,14 @@
-// pub struct Solution;
+pub struct Solution;
 
-// impl Solution {
-//     pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
-//         let mut target = Vec::new();
+impl Solution {
+    #[must_use]
+    pub fn create_target_array(nums: &[i32], index: &[usize]) -> Vec<i32> {
+        let mut target = Vec::new();
 
-//         for (&num, &idx) in nums.iter().zip(index.iter()) {
+        for (&num, &idx) in nums.iter().zip(index.iter()) {
+            target.insert(idx, num);
+        }
 
-//         }
-//     }
-// }
+        target
+    }
+}
