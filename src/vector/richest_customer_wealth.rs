49ee8e93fbdfa07f8a05ad138f5f4e382a3b3290
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-15 18:02:52 +0900

    refactor using standard library

--- src/vector/richest_customer_wealth.rs
+++ src/vector/richest_customer_wealth.rs
@@ -3,13 +3,10 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn maximum_wealth(accounts: &[Vec<i32>]) -> i32 {
-        let mut maximum_wealth = i32::MIN;
-
-        for customer_accounts in accounts {
-            let customer_wealth: i32 = customer_accounts.iter().sum();
-
-            maximum_wealth = maximum_wealth.max(customer_wealth);
-        }
-        maximum_wealth
+        accounts
+            .iter()
+            .map(|customer_accounts| customer_accounts.iter().sum())
+            .max()
+            .unwrap_or(0)
     }
 }
