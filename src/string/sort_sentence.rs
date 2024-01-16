72dea3b8da2918f4850673721230c843d2fd411f
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 16:16:57 +0900

    refactor sort sentences using standard library

--- src/string/sort_sentence.rs
+++ src/string/sort_sentence.rs
@@ -5,14 +5,12 @@ impl Solution {
     pub fn sort_sentence(s: &str) -> String {
         let mut words: Vec<&str> = s.split_whitespace().collect();
 
-        words.sort_by_key(|word| word.chars().last().unwrap_or('O'));
+        words.sort_by_key(|word| word.chars().last().unwrap_or('\0'));
 
-        let reconstructed_sentence: String = words
-            .into_iter()
+        words
+            .iter()
             .map(|word| &word[..word.len() - 1])
             .collect::<Vec<&str>>()
-            .join(" ");
-
-        reconstructed_sentence
+            .join(" ")
     }
 }
