180a4887e96ce3a6913bb54fc79b6c3bca140cdc
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 14:09:46 +0900

    refactor for kids greatest candies using standard library

--- src/numbers/kids_greatest_candies.rs
+++ src/numbers/kids_greatest_candies.rs
@@ -3,13 +3,10 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Vec<bool> {
-        let mut max = 0;
-        let mut ans = Vec::new();
-
-        for &candy in candies {
-            max = std::cmp::max(max, candy);
-            ans.push(candy + extra_candies >= max);
-        }
-        ans
+        let max_candies = candies.iter().copied().max().unwrap_or(0);
+        candies
+            .iter()
+            .map(|&kid_candies| kid_candies + extra_candies >= max_candies)
+            .collect()
     }
 }
