1d7a770ba2fbf759147c2a36be63bdb86276bcca
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 13:37:50 +0900

    refactor for num good pairs using standard library

--- src/numbers/num_good_pairs.rs
+++ src/numbers/num_good_pairs.rs
@@ -4,13 +4,14 @@ use std::collections::HashMap;
 impl Solution {
     #[must_use]
     pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
-        let mut count = 0;
         let mut num_count = HashMap::new();
+        let mut count = 0;
 
         for num in nums {
-            let value = num_count.entry(num).or_insert(0);
-            count += *value;
-            *value += 1;
+            count += *num_count
+                .entry(num)
+                .and_modify(|value| *value += 1)
+                .or_insert(0);
         }
 
         count
