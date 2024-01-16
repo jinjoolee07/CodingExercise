e015c1785406d683126651758ab1fb7e260d3a87
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 14:26:22 +0900

    refactor for smaller than current using standard library

--- examples/example_numbers_smaller.rs
+++ examples/example_numbers_smaller.rs
@@ -1,7 +1,7 @@
 use coding_exercise::numbers::smaller_than_current::Solution;
 
 fn main() {
-    let input = [6, 5, 4, 8];
-    let result = Solution::smaller_than_current(&input);
+    let input = vec![6, 5, 4, 8];
+    let result = Solution::smaller_numbers_than_current(&input);
     println!("{:?}", result);
 }
--- src/numbers/smaller_than_current.rs
+++ src/numbers/smaller_than_current.rs
@@ -1,24 +1,13 @@
 pub struct Solution;
-use std::collections::HashMap;
 
 impl Solution {
     #[must_use]
-    pub fn smaller_than_current(nums: &[i32]) -> Vec<i32> {
-        let mut num_count = HashMap::new();
-        let mut result = Vec::new();
+    pub fn smaller_numbers_than_current(nums: &[i32]) -> Vec<i32> {
+        let mut sorted_nums = nums.to_vec();
+        sorted_nums.sort_unstable();
 
-        for &num in nums {
-            let count = num_count.entry(num).or_insert(0);
-            result.push(*count);
-            *count += 1;
-        }
-
-        for i in 0..result.len() {
-            let num = nums[i];
-            let smaller_count = (0..num).map(|x| num_count.get(&x).unwrap_or(&0)).sum();
-            result[i] = smaller_count;
-        }
-
-        result
+        nums.iter()
+            .map(|&num| i32::try_from(sorted_nums.binary_search(&num).unwrap_or(0)).unwrap_or(0))
+            .collect()
     }
 }
