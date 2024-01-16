3c26ee910caf635d025480e8de1abce36af16d53
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 14:21:29 +0900

    refactor for subtract product sum using standard library

--- src/numbers/subtract_product_sum.rs
+++ src/numbers/subtract_product_sum.rs
@@ -3,17 +3,12 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn subtract_product_and_sum(n: i32) -> i32 {
-        let mut num = n;
-        let mut product = 1;
-        let mut add = 0;
+        let (product, sum) = n
+            .to_string()
+            .chars()
+            .map(|c| i32::try_from(c.to_digit(10).unwrap_or(0)).unwrap_or(0))
+            .fold((1, 0), |(prod, s), digit| (prod * digit, s + digit));
 
-        while num > 0 {
-            let digit = num % 10;
-            product *= digit;
-            add += digit;
-            num /= 10;
-        }
-
-        product - add
+        product - sum
     }
 }
