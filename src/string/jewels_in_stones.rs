526e58f2ba3961ccba33e8238a3c8e319dc21356
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 14:45:41 +0900

    refactor jewels and stones using standard library

--- src/string/jewels_in_stones.rs
+++ src/string/jewels_in_stones.rs
@@ -1,23 +1,17 @@
 pub struct Solution;
 use std::collections::HashSet;
+use std::convert::TryFrom;
 
 impl Solution {
     #[must_use]
     pub fn num_jewels_in_stones(jewels: &str, stones: &str) -> i32 {
-        let mut jewel_set = HashSet::new();
-
-        for jewel in jewels.chars() {
-            jewel_set.insert(jewel);
-        }
-
-        let mut count = 0;
-
-        for stone in stones.chars() {
-            if jewel_set.contains(&stone) {
-                count += 1;
-            }
-        }
-
-        count
+        let jewel_set: HashSet<_> = jewels.chars().collect();
+        i32::try_from(
+            stones
+                .chars()
+                .filter(|&stone| jewel_set.contains(&stone))
+                .count(),
+        )
+        .unwrap_or_default()
     }
 }
