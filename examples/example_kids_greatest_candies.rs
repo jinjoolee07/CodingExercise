a1a0bd698a646a16418bc940fcfd78f7853a7ebc
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 14:09:46 +0900

    refactor for kids greatest candies using standard library

--- examples/example_kids_greatest_candies.rs
+++ examples/example_kids_greatest_candies.rs
@@ -1,7 +1,7 @@
 use coding_exercise::numbers::kids_greatest_candies::Solution;
 
 fn main() {
-    let input = [2, 3, 5, 1, 3];
+    let input = vec![2, 3, 5, 1, 3];
     let n = 3;
     let result = Solution::kids_with_candies(&input, n);
 
