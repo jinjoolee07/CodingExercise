43fe516e8574ed58e45ea75f6d0097816cbc4047
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-16 14:48:22 +0900

    refactor max words in sentence using standard library

--- src/string/max_words_sentence.rs
+++ src/string/max_words_sentence.rs
@@ -2,15 +2,11 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn most_words_found(sentences: Vec<&str>) -> usize {
-        let mut max_word_count = 0;
-
-        for sentence in sentences {
-            let word_count = sentence.split_whitespace().count();
-            if word_count > max_word_count {
-                max_word_count = word_count;
-            }
-        }
-        max_word_count
+    pub fn most_words_found(sentences: &[&str]) -> usize {
+        sentences
+            .iter()
+            .map(|sentence| sentence.split_whitespace().count())
+            .max()
+            .unwrap_or(0)
     }
 }
