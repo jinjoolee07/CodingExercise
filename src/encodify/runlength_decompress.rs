da5fce6867e8f6402d41717925fa48a61e965ebb
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-17 14:08:54 +0900

    refactor decompress runlength using standard library

--- src/encodify/runlength_decompress.rs
+++ src/encodify/runlength_decompress.rs
@@ -3,15 +3,12 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn decompress_rl_elist(nums: &[i32]) -> Vec<i32> {
-        let mut result = Vec::new();
-
-        for i in (0..nums.len()).step_by(2) {
-            let freq = usize::try_from(nums[i]).unwrap_or(1);
-            let val = nums[i + 1];
-
-            result.extend(std::iter::repeat(val).take(freq));
-        }
-
-        result
+        nums.chunks(2)
+            .flat_map(|chunk| {
+                let freq = usize::try_from(chunk[0]).unwrap_or(0);
+                let val = chunk[1];
+                vec![val; freq]
+            })
+            .collect()
     }
 }
