21f6df9f3a3a24e882d9cf96198e778ce475b959
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-17 10:40:05 +0900

    Update example and implementation for min_sum_four_digit

--- examples/example_min_sum_four_digit.rs
+++ examples/example_min_sum_four_digit.rs
@@ -1,7 +1,7 @@
 use coding_exercise::numbers::min_sum_four_digit::Solution;
 
 fn main() {
-    let input = 4009;
+    let input = 2932;
     let result = Solution::minimum_sum(input);
     println!("{:?}", result);
 }
--- src/numbers/min_sum_four_digit.rs
+++ src/numbers/min_sum_four_digit.rs
@@ -2,15 +2,22 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn minimum_sum(num: i32) -> usize {
-        let mut nums: Vec<_> = num
-            .to_string()
-            .chars()
-            .map(|c| c.to_digit(10).unwrap_or(0) as usize)
-            .collect();
+    pub fn minimum_sum(num: i32) -> i32 {
+        let mut digits: Vec<_> = num.to_string().chars().collect();
+        digits.sort_unstable();
 
-        nums.sort_unstable();
+        let (new1, new2) =
+            digits
+                .chunks(2)
+                .fold((String::new(), String::new()), |(mut a1, mut a2), chunk| {
+                    a1.push(chunk[0]);
+                    a2.push(chunk[1]);
+                    (a1, a2)
+                });
 
-        nums[0] + nums[1] + nums[2] + nums[3]
+        let new1 = new1.parse::<i32>().unwrap_or(0);
+        let new2 = new2.parse::<i32>().unwrap_or(0);
+
+        new1 + new2
     }
 }
