c78550b128bba3b3d92724e42f63ce43bbee6479
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-17 11:28:14 +0900

    refactor xored decoder using standard library

--- src/encodify/xored_decoder.rs
+++ src/encodify/xored_decoder.rs
@@ -3,17 +3,11 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn decode(encoded: &[i32], first: i32) -> Vec<i32> {
-        let mut arr = Vec::with_capacity(encoded.len() + 1);
-        arr.push(first);
-
-        for &encode in encoded {
-            if let Some(&last) = arr.last() {
-                arr.push(last ^ encode);
-            } else {
-                return Vec::new();
-            }
-        }
-
+        let mut arr = vec![first];
+        arr.extend(encoded.iter().scan(first, |state, &x| {
+            *state ^= x;
+            Some(*state)
+        }));
         arr
     }
 }
