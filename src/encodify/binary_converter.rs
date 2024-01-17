626309db2f4f576a92685fb77d3db759d3350bd2
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-17 18:49:05 +0900

    refactor binary convert solution using rust standard library

--- src/encodify/binary_converter.rs
+++ src/encodify/binary_converter.rs
@@ -1,35 +1,12 @@
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    #[must_use]
-    pub fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
-
 pub struct Solution;
 
 impl Solution {
     #[must_use]
     pub fn get_decimal_value(vec: &[i32]) -> i32 {
         let mut result = 0;
-        let mut current = None;
 
         for &val in vec {
-            let _node = ListNode::new(val); // Added underscore to indicate intentional unused variable
-            current = Some(Box::new(ListNode { val, next: current }));
-        }
-
-        let mut current = &current;
-        while let Some(node) = current {
-            result = (result << 1) + node.val;
-            current = &node.next;
+            result = (result << 1) | val;
         }
 
         result
