e018190e34cc679a71e52c1846904f889b7b85f6
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-17 15:47:16 +0900

    refactor shuffle_string

--- src/encodify/shuffle_string.rs
+++ src/encodify/shuffle_string.rs
@@ -3,12 +3,15 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn restore_string(s: &str, indices: &[usize]) -> String {
-        let mut result = vec![' '; s.len()];
-
-        for (i, &index) in s.chars().zip(indices.iter()) {
-            result[index] = i;
-        }
-
-        result.into_iter().collect()
+        indices
+            .iter()
+            .copied()
+            .zip(s.chars())
+            .fold(vec![' '; s.len()], |mut result, (index, char)| {
+                result[index] = char;
+                result
+            })
+            .into_iter()
+            .collect()
     }
 }
