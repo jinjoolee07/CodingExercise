af0d3c81c45b23a0c0b695e85c73c10db60ec01b
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-19 15:49:54 +0900

    new fixes for concatenation, build array, and richest customer solutions

--- src/vector/concatenation_of_array.rs
+++ src/vector/concatenation_of_array.rs
@@ -3,8 +3,6 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn get_concatenation(nums: &[i32]) -> Vec<i32> {
-        let mut ans = nums.to_vec();
-        ans.extend_from_slice(nums);
-        ans
+        nums.iter().copied().chain(nums.iter().copied()).collect()
     }
 }
