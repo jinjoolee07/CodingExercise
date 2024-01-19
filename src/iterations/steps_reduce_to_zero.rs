e7d278cd7af3a8ef2393b6ad9f3b24b58c78ff48
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-19 09:09:58 +0900

    refactor steps reduce to zero using standard library

--- src/iterations/steps_reduce_to_zero.rs
+++ src/iterations/steps_reduce_to_zero.rs
@@ -2,17 +2,7 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn number_of_steps(mut num: i32) -> i32 {
-        let mut steps = 0;
-
-        while num != 0 {
-            if num % 2 == 0 {
-                num /= 2;
-            } else {
-                num -= 1;
-            }
-            steps += 1;
-        }
-        steps
+    pub fn number_of_steps(num: i32) -> u32 {
+        num.count_ones() + (31 - num.leading_zeros())
     }
 }
