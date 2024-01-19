52ce09670551c5055485fe06fac4e2af1099cad8
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-19 16:39:47 +0900

    refactor min moves seat solution

--- src/iterations/min_moves_seat_all.rs
+++ src/iterations/min_moves_seat_all.rs
@@ -2,17 +2,14 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn min_moves_to_seat(seats: Vec<i32>, students: Vec<i32>) -> i32 {
-        let mut move_count = 0;
-        let mut seat = seats;
-        let mut student = students;
+    pub fn min_moves_to_seat(mut seats: Vec<i32>, mut students: Vec<i32>) -> i32 {
+        seats.sort_unstable();
+        students.sort_unstable();
 
-        seat.sort_unstable();
-        student.sort_unstable();
-
-        for (s, st) in student.iter().zip(seat.iter()) {
-            move_count += (s - st).abs();
-        }
-        move_count
+        students
+            .iter()
+            .zip(seats.iter())
+            .map(|(&student_pos, &seat_pos)| (student_pos - seat_pos).abs())
+            .sum()
     }
 }
