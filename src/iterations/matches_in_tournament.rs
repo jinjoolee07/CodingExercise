f07bce694b7933065dd528e5b3139cce6c120318
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-19 16:08:56 +0900

    refactor matches in tournament

--- src/iterations/matches_in_tournament.rs
+++ src/iterations/matches_in_tournament.rs
@@ -2,14 +2,7 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn number_of_matches(mut n: i32) -> i32 {
-        let mut matches_played = 0;
-
-        while n > 1 {
-            matches_played += n / 2;
-            n = (n + 1) / 2;
-        }
-
-        matches_played
+    pub fn number_of_matches(n: i32) -> i32 {
+        n - 1
     }
 }
