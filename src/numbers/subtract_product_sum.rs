5d6301778e6754b5911591b81228db5c44ab7a50
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-23 11:36:19 +0900

    fixed subtract product sum to use expect rather than upwrap since we are confident in conversions

--- src/numbers/subtract_product_sum.rs
+++ src/numbers/subtract_product_sum.rs
@@ -2,11 +2,19 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
+    /// # Panics
+    /// Panics if the conversion from char to digit or digit to i32 fails.
     pub fn subtract_product_and_sum(n: i32) -> i32 {
         let (product, sum) = n
             .to_string()
             .chars()
-            .map(|c| i32::try_from(c.to_digit(10).unwrap_or(0)).unwrap_or(0))
+            .map(|c| {
+                i32::try_from(
+                    c.to_digit(10)
+                        .expect("Conversion from char to digit failed"),
+                )
+                .expect("Conversion from digit to i32 failed")
+            })
             .fold((1, 0), |(prod, s), digit| (prod * digit, s + digit));
 
         product - sum
