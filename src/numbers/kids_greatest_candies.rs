ad03fb57eba5d2201582f9c527ca14d85e333ff9
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-23 09:43:50 +0900

    fixed kids with greatest candiese to avoid using copied functions and handling empty slice.

--- src/numbers/kids_greatest_candies.rs
+++ src/numbers/kids_greatest_candies.rs
@@ -2,11 +2,12 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Vec<bool> {
-        let max_candies = candies.iter().copied().max().unwrap_or(0);
-        candies
-            .iter()
-            .map(|&kid_candies| kid_candies + extra_candies >= max_candies)
-            .collect()
+    pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Option<Vec<bool>> {
+        candies.iter().max().map(|&max_candies| {
+            candies
+                .iter()
+                .map(|&kid_candies| kid_candies + extra_candies >= max_candies)
+                .collect()
+        })
     }
 }
