1ceb7f094bff79ec7e098b922256a7ab76124714
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-24 15:54:20 +0900

    used collect instead of Vec::new() and push which simplies the code more for smaller than current solution

--- src/numbers/smaller_than_current.rs
+++ src/numbers/smaller_than_current.rs
@@ -3,13 +3,8 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn smaller_numbers_than_current(nums: &[i32]) -> Vec<usize> {
-        let mut result = Vec::new();
-
-        for i in 0..nums.len() {
-            let count_smaller = nums.iter().filter(|&&num| num < nums[i]).count();
-            result.push(count_smaller);
-        }
-
-        result
+        (0..nums.len())
+            .map(|i| nums.iter().filter(|&&num| num < nums[i]).count())
+            .collect()
     }
 }
