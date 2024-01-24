9322d0d97003d0137700412996f497f76b8eae03
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-24 16:35:27 +0900

    used let-else instead of if let

--- src/numbers/smaller_than_current.rs
+++ src/numbers/smaller_than_current.rs
@@ -1,10 +1,16 @@
 pub struct Solution;
 
 impl Solution {
+    /// # Panics
+    /// Panics if attempting to access an index out of bounds.
     #[must_use]
     pub fn smaller_numbers_than_current(nums: &[i32]) -> Vec<usize> {
         (0..nums.len())
-            .map(|i| nums.iter().filter(|&&num| num < nums[i]).count())
+            .map(|i| {
+                nums.iter()
+                    .filter(|&&num| num < *nums.get(i).expect("Indexing out of bounds"))
+                    .count()
+            })
             .collect()
     }
 }
