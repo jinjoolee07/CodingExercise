dd60ea3c9794dde91f87cfb09b4efa97941f2fba
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-24 15:41:58 +0900

    refactored smaller than current solution using filter and count

--- src/numbers/smaller_than_current.rs
+++ src/numbers/smaller_than_current.rs
@@ -1,25 +1,15 @@
-use std::collections::HashMap;
-
 pub struct Solution;
 
 impl Solution {
     #[must_use]
-    /// # Panics
-    /// This function may panic if the binary search result is not within the bounds of the sorted array.
-    pub fn smaller_numbers_than_current(nums: &[i32]) -> Vec<i32> {
-        let mut count_map = HashMap::new();
-        let mut sorted_nums = nums.to_vec();
-        sorted_nums.sort_unstable();
+    pub fn smaller_numbers_than_current(nums: &[i32]) -> Vec<usize> {
+        let mut result = Vec::new();
 
-        for (i, &num) in sorted_nums.iter().enumerate() {
-            count_map.entry(num).or_insert(i);
+        for i in 0..nums.len() {
+            let count_smaller = nums.iter().filter(|&&num| num < nums[i]).count();
+            result.push(count_smaller);
         }
 
-        nums.iter()
-            .map(|&num| {
-                i32::try_from(*count_map.get(&num).unwrap())
-                    .expect("Count retrieval from HashMap should always return Some value")
-            })
-            .collect()
+        result
     }
 }
