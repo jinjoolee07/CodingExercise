4d6a01881a09aba7f4bf22a81345bc052dc413d2
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-26 08:52:34 +0900

    Refactor shuffle string solution for simplicity
    Revised the code to utilize a straight forward iteration over indices, eliminating unnecessary functions like copied, zip, fold, and into_iter

--- src/encodify/shuffle_string.rs
+++ src/encodify/shuffle_string.rs
@@ -3,15 +3,12 @@ pub struct Solution;
 impl Solution {
     #[must_use]
     pub fn restore_string(s: &str, indices: &[usize]) -> String {
-        indices
-            .iter()
-            .copied()
-            .zip(s.chars())
-            .fold(vec![' '; s.len()], |mut result, (index, char)| {
-                result[index] = char;
-                result
-            })
-            .into_iter()
-            .collect()
+        let mut result = vec![' '; s.len()];
+
+        for (index, char) in indices.iter().zip(s.chars()) {
+            result[*index] = char;
+        }
+
+        result.into_iter().collect()
     }
 }
