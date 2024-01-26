2320726561c59c8e924de5e47f8ee5b380eafae6
jinjoolee07 <jinjoolee07@gmail.com>
2024-01-26 15:10:22 +0900

    Refactoed matches in tournament with better logic

--- src/iterations/matches_in_tournament.rs
+++ src/iterations/matches_in_tournament.rs
@@ -2,7 +2,12 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn number_of_matches(n: i32) -> i32 {
-        n - 1
+    pub fn number_of_matches(mut n: i32) -> i32 {
+        let mut matches_played = 0;
+        while n > 1 {
+            matches_played += n / 2;
+            n = (n + 1) / 2;
+        }
+        matches_played
     }
 }
